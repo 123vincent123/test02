cbd16644fe4dc91e52522fd16badae2019359bab
heming <5922068038@qq.com>
2021-07-16 22:36:18 +0800

    这是我的第三次提交

--- springboot1/src/main/java/a/HelloController.java
+++ springboot1/src/main/java/a/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
     @RequestMapping(value = "/a", method = {RequestMethod.GET})
     @ResponseBody
     public String successModel(HttpServletRequest request, HttpServletResponse reponse) {
-        return "success555555555555555555555555";
+        return "success5";
     }
 }
